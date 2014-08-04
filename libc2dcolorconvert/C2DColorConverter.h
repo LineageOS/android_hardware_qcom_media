0ac6d80ebbc99c24f92893656a0c93dc4def8d57
Shalaj Jain <shalajj@codeaurora.org>
2014-08-04 21:56:38 +0200

    libc2dcolorconvert: Remove dependency on stagefright
    No need to include ColorConvert.h
    
    Change-Id: I7f5e20bd6551a4c85a0024dbf280fd00c5566149

--- libc2dcolorconvert/C2DColorConverter.h
+++ libc2dcolorconvert/C2DColorConverter.h
@@ -31,10 +31,7 @@
 #define C2D_ColorConverter_H_
 
 #include <c2d2.h>
-#include <ColorConverter.h>
 #include <sys/types.h>
-#include <string.h>
-#include <errno.h>
 
 typedef C2D_STATUS (*LINK_c2dCreateSurface)( uint32 *surface_id,
         uint32 surface_bits,
