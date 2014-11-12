6d2a0b24d35fcd98f852629487e06ea1573d3813
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-12 20:18:40 +0530

    media: Use kernel headers
    Change-Id: I7a238b7c105d58eb6ec7794ec7a6af37f63c4cb0

--- msm8974/mm-video-v4l2/vidc/vdec.mk
+++ msm8974/mm-video-v4l2/vidc/vdec.mk
@@ -88,6 +88,8 @@ endif
 libOmxVdec-def += -DFLEXYUV_SUPPORTED
 libOmxVdec-def += -DADAPTIVE_PLAYBACK_SUPPORTED
 
+vdec-inc       = $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
 # ---------------------------------------------------------------------------------
 # 			Make the Shared library (libOmxVdec)
 # ---------------------------------------------------------------------------------
@@ -140,6 +142,7 @@ LOCAL_SRC_FILES         += common/src/vidc_color_converter.cpp
 LOCAL_CLANG_CFLAGS      += -Wno-pointer-bool-conversion
 
 LOCAL_CFLAGS            += -Wno-error
+LOCAL_ADDITIONAL_DEPENDENCIES  := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -174,7 +177,11 @@ LOCAL_SRC_FILES         += vdec/src/hevc_utils.cpp
 LOCAL_SRC_FILES         += common/src/extra_data_handler.cpp
 LOCAL_SRC_FILES         += common/src/vidc_color_converter.cpp
 
+<<<<<<< HEAD
 LOCAL_CFLAGS            += -Wno-error
+=======
+LOCAL_ADDITIONAL_DEPENDENCIES  := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+>>>>>>> f7548b7a0... media: Use kernel headers
 
 #include $(BUILD_SHARED_LIBRARY)
 
@@ -217,6 +224,8 @@ LOCAL_C_INCLUDES                := $(mm-vdec-drv-test-inc)
 
 LOCAL_SRC_FILES                 := vdec/src/message_queue.c
 LOCAL_SRC_FILES                 += vdec/test/decoder_driver_test.c
+
+LOCAL_ADDITIONAL_DEPENDENCIES  := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 #include $(BUILD_EXECUTABLE)
 
