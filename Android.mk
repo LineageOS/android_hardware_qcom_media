65c2e1921a3f131f0583db74b61e53d2dd23d157
Ethan Chen <intervigil@gmail.com>
2015-02-23 22:59:57 -0500

    media: Use project pathmap
    Change-Id: I4f2e1f666deded6d53f23c97718b4246b48cf780
    (cherry picked from commit b902a13e9c1302367b03dc271a23e7a72f3f7d05)

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
+
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -20,4 +22,5 @@ ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
 
+endif
 endif
