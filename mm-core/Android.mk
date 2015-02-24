b341b02cbb9a9a6f79b9cb3f014c4a6452d4a08b
Ethan Chen <intervigil@gmail.com>
2015-02-23 22:59:57 -0500

    media: Use project pathmap
    Change-Id: I4f2e1f666deded6d53f23c97718b4246b48cf780

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -71,7 +71,7 @@ LOCAL_COPY_HEADERS      += inc/QCMetaData.h
 #===============================================================================
 
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/src/common
-LOCAL_C_INCLUDES        += $(TOP)/hardware/qcom/media/libplatformconfig
+LOCAL_C_INCLUDES        += $(call project-path-for,qcom-media)/libplatformconfig
 
 LOCAL_HEADER_LIBRARIES := \
         libutils_headers \
@@ -116,7 +116,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/src/common
-LOCAL_C_INCLUDES        += $(TOP)/hardware/qcom/media/libplatformconfig
+LOCAL_C_INCLUDES        += $(call project-path-for,qcom-media)/libplatformconfig
 
 LOCAL_HEADER_LIBRARIES := \
         libutils_headers \
