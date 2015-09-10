08e6447372caca0ed8600dc7d822b8d771680f5e
Abdulla Anam <abdullahanam@codeaurora.org>
2015-09-10 17:14:10 +0530

    mm-video-v4l2: venc: limit pixels value in input buffers.
    Limit pixel values from 0 to 252 for luma and 0 to 253 for chroma
    in input YUV buffer before sending it to video hardware to avoid
    output corruption in mpeg4 encoding format on msm8976.
    
    Change-Id: Ie37e160d7210065e7349903e2bcee4e89902ed8b
    Signed-off-by: Chippa-a <bagirovvusal99@gmail.com>

--- mm-video-v4l2/vidc/venc/inc/video_encoder_device_v4l2.h
+++ mm-video-v4l2/vidc/venc/inc/video_encoder_device_v4l2.h
@@ -56,6 +56,11 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define MAX_AVC_HP_LAYERS (4)
 #define MAX_V4L2_BUFS 64 //VB2_MAX_FRAME
 
+extern "C" {
+    void neon_clip_luma_chroma(unsigned char *, unsigned char *,
+            unsigned int, unsigned int, unsigned int, unsigned int);
+}
+
 enum hier_type {
     HIER_NONE = 0x0,
     HIER_P = 0x1,
@@ -586,6 +591,7 @@ class venc_dev
         OMX_ERRORTYPE venc_set_temporal_layers(OMX_VIDEO_PARAM_ANDROID_TEMPORALLAYERINGTYPE *pTemporalParams, bool istemporalConfig = false);
         OMX_ERRORTYPE venc_set_temporal_layers_internal();
         bool venc_set_iframesize_type(QOMX_VIDEO_IFRAMESIZE_TYPE type);
+        void venc_clip_luma_chroma(int fd, OMX_U32 offset, OMX_U32 size);
 
 #ifdef MAX_RES_1080P
         OMX_U32 pmem_free();
@@ -614,6 +620,7 @@ class venc_dev
         bool is_searchrange_set;
         bool enable_mv_narrow_searchrange;
         int supported_rc_modes;
+        char m_platform[OMX_MAX_STRINGNAME_SIZE];
         bool is_thulium_v1;
         bool camera_mode_enabled;
         OMX_BOOL low_latency_mode;
