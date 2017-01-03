a0ec1336cdb3162fab79f8d5757a3404d95dde18
Surajit Podder <spodder@codeaurora.org>
2017-01-03 15:16:12 +0530

    mm-video-v4l2: Add support for encoder input fps profiling
    Add support encoder input fps profiling, bypassing encoding
    
    Change-Id: I30003806e8613f6dd0b5a84eed67c09fa86558a8

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -270,7 +270,11 @@ omx_video::omx_video():
     m_event_port_settings_sent(false),
     hw_overload(false),
     m_graphicbuffer_size(0),
-    m_buffer_freed(0)
+    m_buffer_freed(0),
+    profile_mode(false),
+    profile_frame_count(0),
+    profile_start_time(0),
+    profile_last_time(0)
 {
     DEBUG_PRINT_HIGH("omx_video(): Inside Constructor()");
     memset(&m_cmp,0,sizeof(m_cmp));
@@ -295,9 +299,11 @@ omx_video::omx_video():
 
     memset(m_platform, 0, sizeof(m_platform));
 #ifdef _ANDROID_
-    char platform_name[PROPERTY_VALUE_MAX] = {0};
-    property_get("ro.board.platform", platform_name, "0");
-    strlcpy(m_platform, platform_name, sizeof(m_platform));
+    char property_value[PROPERTY_VALUE_MAX] = {0};
+    property_get("ro.board.platform", property_value, "0");
+    strlcpy(m_platform, property_value, sizeof(m_platform));
+    property_get("vendor.vidc.enc.profile.in", property_value, "0");
+    profile_mode = !!atoi(property_value);
 #endif
 
     pthread_mutex_init(&m_buf_lock, NULL);
@@ -336,6 +342,10 @@ omx_video::~omx_video()
             m_fbd_count);
 
     pthread_mutex_destroy(&m_buf_lock);
+    if (profile_mode && (profile_start_time < profile_last_time)) {
+        DEBUG_PRINT_HIGH("Input frame rate = %f",
+            ((profile_frame_count - 1) * 1e6) / (profile_last_time - profile_start_time));
+    }
     DEBUG_PRINT_HIGH("omx_video: Destructor exit");
     DEBUG_PRINT_HIGH("Exiting OMX Video Encoder ...");
 }
@@ -3900,6 +3910,22 @@ OMX_ERRORTYPE  omx_video::empty_this_buffer(OMX_IN OMX_HANDLETYPE         hComp,
     post_event ((unsigned long)hComp,(unsigned long)buffer,m_input_msg_id);
     return OMX_ErrorNone;
 }
+
+bool omx_video::profile_etb() {
+    if (profile_mode) {
+        struct timeval act_time = {0, 0};
+        gettimeofday(&act_time, NULL);
+        if (profile_start_time == 0) {
+            profile_start_time = (act_time.tv_usec + act_time.tv_sec * 1e6);
+        } else {
+            profile_last_time = (act_time.tv_usec + act_time.tv_sec * 1e6);
+        }
+        profile_frame_count++;
+        return true;
+    }
+    return false;
+}
+
 /* ======================================================================
    FUNCTION
    omx_video::empty_this_buffer_proxy
@@ -3932,6 +3958,11 @@ OMX_ERRORTYPE  omx_video::empty_this_buffer_proxy(OMX_IN OMX_HANDLETYPE  hComp,
     if (buffer == NULL) {
         DEBUG_PRINT_ERROR("ERROR: ETBProxy: Invalid buffer[%p]", buffer);
         return OMX_ErrorBadParameter;
+    }
+
+    if (profile_etb()) {
+        m_pCallbacks.EmptyBufferDone(hComp,m_app_data,buffer);
+        return OMX_ErrorNone;
     }
 
     // Buffer sanity checks
@@ -4948,6 +4979,11 @@ OMX_ERRORTYPE  omx_video::empty_this_buffer_opaque(OMX_IN OMX_HANDLETYPE hComp,
     if (buffer == NULL) {
         DEBUG_PRINT_ERROR("ERROR: ETBProxyA: Invalid buffer[%p]",buffer);
         return OMX_ErrorBadParameter;
+    }
+
+    if (profile_etb()) {
+        m_pCallbacks.EmptyBufferDone(hComp,m_app_data,buffer);
+        return OMX_ErrorNone;
     }
 
     if (!dev_buffer_ready_to_queue(buffer)) {
