69d7c16cd92ecaa9828e54e4a6ed77948bd98739
Bruno Martins <bgcngm@gmail.com>
2017-02-28 22:45:31 +0000

    Merge tag 'LA.UM.5.5.r1-03400-8x96.0' into cm-14.1-caf-8996
    "LA.UM.5.5.r1-03400-8x96.0"
    
    Change-Id: Iecc627fc0d9bad844531d57696dadf3f79997c42

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2010-2016, Linux Foundation. All rights reserved.
+Copyright (c) 2010-2017, Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
