407aedd1cf93706b5966fdcf27664768f09c070a
Jiyong Park <jiyong@google.com>
2017-03-22 20:43:05 +0900

    Move QCOM mm codecs to vendor partition
    Bug: 36500740
    Test: camcorder recording, video play should work as before.
    libOmxVdec.so and libOmxVenc.so should be loaded into the media codec
    process.
    Change-Id: Id6e349645adc0be3a4873afc1812679f0e710b0b

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -44,5 +44,7 @@ LOCAL_SHARED_LIBRARIES :=       \
 
 LOCAL_MODULE := libstagefrighthw
 
+LOCAL_PROPRIETARY_MODULE := true
+
 include $(BUILD_SHARED_LIBRARY)
 
--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -128,6 +128,7 @@ endif
 
 LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
+LOCAL_PROPRIETARY_MODULE        := true
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 
@@ -170,6 +171,7 @@ ifeq ($(call is-board-platform-in-list,msm8974 msm8610 apq8084 mpq8092 msm8226),
 
 LOCAL_MODULE                    := libOmxVdecHevc
 LOCAL_MODULE_TAGS               := optional
+LOCAL_PROPRIETARY_MODULE        := true
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 
@@ -212,6 +214,7 @@ mm-vdec-test-inc    += $(vdec-inc)
 
 LOCAL_MODULE                    := mm-vdec-omx-test
 LOCAL_MODULE_TAGS               := optional
+LOCAL_PROPRIETARY_MODULE        := true
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
 LOCAL_C_INCLUDES                := $(mm-vdec-test-inc)
 
@@ -235,6 +238,7 @@ mm-vdec-drv-test-inc    += $(vdec-inc)
 
 LOCAL_MODULE                    := mm-video-driver-test
 LOCAL_MODULE_TAGS               := optional
+LOCAL_PROPRIETARY_MODULE        := true
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
 LOCAL_C_INCLUDES                := $(mm-vdec-drv-test-inc)
 LOCAL_PRELINK_MODULE            := false
