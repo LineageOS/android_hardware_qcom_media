7e34247664ab5da3f908af8333a194b06b7b5741
Sujay Kulkarni <sukulk@codeaurora.org>
2017-04-04 17:40:15 -0700

    Updated appropriate target name
    Change-Id: Id0040c1844914edd85044b48e91a7ab9503971f0

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -24,7 +24,7 @@ else ifeq ($(TARGET_BOARD_PLATFORM),msm8998)
 MM_CORE_TARGET = msm8998
 else ifeq ($(TARGET_BOARD_PLATFORM),sdm660)
 MM_CORE_TARGET = sdm660
-else ifeq ($(filter $(TARGET_BOARD_PLATFORM), sdm845 msmskunk),$(TARGET_BOARD_PLATFORM))
+else ifeq ($(filter $(TARGET_BOARD_PLATFORM), sdm845),$(TARGET_BOARD_PLATFORM))
 MM_CORE_TARGET = sdm845
 else
 MM_CORE_TARGET = default
@@ -77,7 +77,7 @@ LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
 LOCAL_SRC_FILES         := src/common/omx_core_cmp.cpp
 LOCAL_SRC_FILES         += src/common/qc_omx_core.c
-ifneq (,$(filter msm8996 msm8998 sdm660 msmskunk sdm845,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8996 msm8998 sdm660 sdm845,$(TARGET_BOARD_PLATFORM)))
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/registry_table_android.c
 else
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/qc_registry_table_android.c
@@ -101,7 +101,7 @@ LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
 LOCAL_SRC_FILES         := src/common/omx_core_cmp.cpp
 LOCAL_SRC_FILES         += src/common/qc_omx_core.c
-ifneq (,$(filter msm8996 msm8998 sdm660 msmskunk sdm845,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8996 msm8998 sdm660 sdm845,$(TARGET_BOARD_PLATFORM)))
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/registry_table.c
 else
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/qc_registry_table.c
