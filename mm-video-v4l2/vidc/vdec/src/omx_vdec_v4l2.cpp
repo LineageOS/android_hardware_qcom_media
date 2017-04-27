c00681fd6c7bc5cbe5a914a1da994da20f008a74
Abdulla Anam <abdullahanam@codeaurora.org>
2017-04-27 13:24:07 +0530

    mm-video-v4l2: vidc: Fix set_callbacks return type.
    set_callbacks is implemented where the client's callback functions
    are registered in omx component. Return Omx_ErrorNone.
    
    Change-Id: I81b7f0bd449b8b48fb53a2c0bc96b868ba6f8404

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -7996,11 +7996,15 @@ OMX_ERRORTYPE  omx_vdec::set_callbacks(OMX_IN OMX_HANDLETYPE        hComp,
         OMX_IN OMX_PTR             appData)
 {
     (void) hComp;
+
+    if (!callbacks)
+        return OMX_ErrorBadParameter;
+
     m_cb       = *callbacks;
     DEBUG_PRINT_LOW("Callbacks Set %p %p %p",m_cb.EmptyBufferDone,\
             m_cb.EventHandler,m_cb.FillBufferDone);
     m_app_data =    appData;
-    return OMX_ErrorNotImplemented;
+    return OMX_ErrorNone;
 }
 
 /* ======================================================================
--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -4145,11 +4145,15 @@ OMX_ERRORTYPE  omx_video::set_callbacks(OMX_IN OMX_HANDLETYPE        hComp,
         OMX_IN OMX_PTR             appData)
 {
     (void)hComp;
+
+    if (!callbacks)
+       return OMX_ErrorBadParameter;
+
     m_pCallbacks       = *callbacks;
     DEBUG_PRINT_LOW("Callbacks Set %p %p %p",m_pCallbacks.EmptyBufferDone,\
             m_pCallbacks.EventHandler,m_pCallbacks.FillBufferDone);
     m_app_data =    appData;
-    return OMX_ErrorNotImplemented;
+    return OMX_ErrorNone;
 }
 
 
