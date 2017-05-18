1d02f68b14d679ea08496bb01cde267d14140974
Uma Mehta <umamehta@codeaurora.org>
2017-05-18 16:53:52 +0530

    media: Move Video HAL libraries to vendor image.
    Move video HAL libraries to vendor image
    with flag LOCAL_VENDOR_MODULE
    
    Change-Id: Ie9058b9bfc1148bed53e4496c58a874698f1f6d1

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -96,10 +96,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
-LOCAL_PROPRIETARY_MODULE        := true
-LOCAL_MODULE_OWNER              := qti
-LOCAL_MODULE_PATH_32            := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MODULE_PATH_64            := $(TARGET_OUT_VENDOR)/lib64
+LOCAL_VENDOR_MODULE             := true
 LOCAL_CFLAGS                    := $(libmm-vdec-def) -Werror
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-vdec-add-dep)
@@ -127,10 +124,7 @@ ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_NEED_SW_VDEC)),true)
 
 LOCAL_MODULE                  := libOmxSwVdec
 LOCAL_MODULE_TAGS             := optional
-LOCAL_PROPRIETARY_MODULE      := true
-LOCAL_MODULE_OWNER            := qti
-LOCAL_MODULE_PATH_32          := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MODULE_PATH_64          := $(TARGET_OUT_VENDOR)/lib64
+LOCAL_VENDOR_MODULE           := true
 LOCAL_CFLAGS                  := $(libmm-vdec-def)
 LOCAL_C_INCLUDES              += $(libmm-vdec-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(libmm-vdec-add-dep)
