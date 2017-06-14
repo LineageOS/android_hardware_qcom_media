b64cbc1f2f9acb94e1a07defb53c34a55ecbf80b
Umesh Pandey <umeshp@codeaurora.org>
2017-06-13 18:04:12 -0700

    mm-video-v4l2: vdec: Color Metadata based on client params
    - For VP9 HDR and HLG, hdr info and coloraspects are present in
      the container rather than the bitstream, hence client sets the color aspects and
      hdr info via setConfig of OMX_QTIIndexConfigDescribeColorAspects and
      OMX_QTIIndexConfigDescribeHDRColorInfo. Use the client coloraspects and hdr
      info when converting to color metadata.
    - ColorMetaData is now calculated from coloraspects. This helps
      in removing the colormetadata in future when android dataspace
      is enabled.
    - Deprecate COLOR_SPACE and instead use COLOR_METADATA to inform
      color aspects and hdr info on the gralloc metadata.
    
    CRs-Fixed: 2064193
    Change-Id: I2054aed403808dde7d4a64ca1baa040dd418a1cd

--- mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
+++ mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
@@ -514,6 +514,10 @@ struct extradata_info {
 
 typedef std::unordered_map <int, int> ColorSubMapping;
 typedef std::unordered_map <int, ColorSubMapping> DecColorMapping;
+typedef std::unordered_map <enum ColorAspects::Primaries, ColorPrimaries> PrimariesMap;
+typedef std::unordered_map <enum ColorAspects::Transfer, GammaTransfer> TransferMap;
+typedef std::unordered_map <enum ColorAspects::MatrixCoeffs, MatrixCoEfficients> MatrixCoeffMap;
+typedef std::unordered_map <enum ColorAspects::Range, ColorRange> RangeMap;
 
 // OMX video decoder class
 class omx_vdec: public qc_omx_component
@@ -871,18 +875,14 @@ class omx_vdec: public qc_omx_component
         void handle_extradata_secure(OMX_BUFFERHEADERTYPE *p_buf_hdr);
         void handle_extradata(OMX_BUFFERHEADERTYPE *p_buf_hdr);
         void convert_color_space_info(OMX_U32 primaries, OMX_U32 range,
-            OMX_U32 transfer, OMX_U32 matrix, ColorSpace_t *color_space,
+            OMX_U32 transfer, OMX_U32 matrix,
             ColorAspects *aspects);
-        bool handle_color_space_info(void *data,
-                                     ColorSpace_t *color_space,
-                                     ColorMetaData* color_mdata,
-                                     bool& set_color_aspects_only);
-        void set_colorspace_in_handle(ColorSpace_t color, unsigned int buf_index);
+        bool handle_color_space_info(void *data);
         void print_debug_color_aspects(ColorAspects *aspects, const char *prefix);
         void print_debug_hdr_color_info(HDRStaticInfo *hdr_info, const char *prefix);
         void print_debug_hdr_color_info_mdata(ColorMetaData* color_mdata);
-        bool handle_content_light_level_info(void* data, ContentLightLevel* light_level_mdata);
-        bool handle_mastering_display_color_info(void* data, MasteringDisplay* mastering_display_mdata);
+        bool handle_content_light_level_info(void* data);
+        bool handle_mastering_display_color_info(void* data);
         void print_debug_extradata(OMX_OTHER_EXTRADATATYPE *extra);
         void set_colormetadata_in_handle(ColorMetaData *color_mdata, unsigned int buf_index);
         void prepare_color_aspects_metadata(OMX_U32 primaries, OMX_U32 range,
@@ -1201,9 +1201,7 @@ class omx_vdec: public qc_omx_component
         // HDRStaticInfo defined in HardwareAPI.h
         DescribeHDRStaticInfoParams m_client_hdr_info;
         DescribeHDRStaticInfoParams m_internal_hdr_info;
-        bool m_change_client_hdr_info;
-        pthread_mutex_t m_hdr_info_client_lock;
-        ColorMetaData m_color_mdata;
+
 
         OMX_U32 operating_frame_rate;
 
@@ -1382,6 +1380,18 @@ class omx_vdec: public qc_omx_component
 
         // list of extensions is not mutable after initialization
         const VendorExtensionStore mVendorExtensionStore;
+
+        // Map of ColorAspects (VideoAPI.h) to ColorMetaData (color_metadata.h)
+        PrimariesMap mPrimariesMap;
+        TransferMap mTransferMap;
+        MatrixCoeffMap mMatrixCoeffMap;
+        RangeMap mColorRangeMap;
+
+        void init_color_aspects_map();
+        void convert_color_aspects_to_metadata(ColorAspects& aspects, ColorMetaData &color_mdata);
+        void convert_hdr_info_to_metadata(HDRStaticInfo& hdr_info, ColorMetaData &color_mdata);
+        void get_preferred_color_aspects(ColorAspects& preferredColorAspects);
+        void get_preferred_hdr_info(HDRStaticInfo& preferredHDRInfo);
 };
 
 enum instance_state {
