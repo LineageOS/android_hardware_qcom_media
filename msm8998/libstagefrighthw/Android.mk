9c57826f3fde7833ddf5bc8ff9c41f79b8953308
Jiyong Park <jiyong@google.com>
2017-07-03 21:38:32 +0900

    Build libstagefrighthw with BOARD_VNDK_VERSION
    Instead of relying on the global include paths, use the new headers lib
    'media_plugin_headers'.
    
    Bug: 37342627
    Test: BOARD_VNDK_VERSION=current m -j libstagefrighthw
    Change-Id: I09fc6c053a0b18c6a0c8f6d017bc001d50d11ca2

--- msm8998/libstagefrighthw/Android.mk
+++ msm8998/libstagefrighthw/Android.mk
@@ -48,6 +48,9 @@ LOCAL_SHARED_LIBRARIES :=       \
         libcutils               \
         libdl                   \
 
+LOCAL_HEADER_LIBRARIES := \
+        media_plugin_headers
+
 LOCAL_MODULE := libstagefrighthw
 
 LOCAL_VENDOR_MODULE := true
