bb373d11270ecb76e56dd5286f5960662ca1230e
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2017-07-13 15:11:34 +0530

    mm-video-v4l2: venc: Add offset to buffer address while dump
    - Add offset to the buffer address while dump, For vp8
      data starts after offset.
    
    CRs-Fixed: 2075098
    Change-Id: I3aed881bc628b69ea4ba60aad945648627394af5

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -4514,7 +4514,7 @@ OMX_ERRORTYPE omx_video::fill_buffer_done(OMX_HANDLETYPE hComp,
             m_fbd_count++;
 
             if (dev_get_output_log_flag()) {
-                dev_output_log_buffers((const char*)buffer->pBuffer, buffer->nFilledLen);
+                dev_output_log_buffers((const char*)buffer->pBuffer + buffer->nOffset, buffer->nFilledLen);
             }
         }
         if (buffer->nFlags & OMX_BUFFERFLAG_EXTRADATA) {
