ccaa0abcddaeeec966a4ef82efdf87b944db6704
Deepak Kushwah <dkushwah@codeaurora.org>
2017-09-04 15:52:49 +0530

    mm-video-v4l2: vidc: Restrict PQ upto 60 FPS
    Restrain component to enable PQ beyond 60 FPS,
    In current scenario PQ can be enabled upto 280
    FPS for 720p resolution, restrict it to 60 due
    to performance limitations.
    
    Change-Id: I313e8e4fca4178622424303ef2a3792f46780964

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -80,6 +80,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define SZ_4K 0x1000
 #define SZ_1M 0x100000
+#define MAX_FPS_PQ 60
 
 /* MPEG4 profile and level table*/
 static const unsigned int mpeg4_profile_level_table[][MAX_PROFILE_PARAMS]= {
@@ -8220,8 +8221,7 @@ bool venc_dev::venc_check_for_pq(void)
         m_pq.caps.max_width * m_pq.caps.max_height;
 
     frame_rate_supported =
-        (m_sVenc_cfg.fps_num / m_sVenc_cfg.fps_den) <=
-        (m_pq.caps.max_mb_per_sec / ((m_sVenc_cfg.input_height * m_sVenc_cfg.input_width) / 256));
+        (operating_rate >> 16) <= MAX_FPS_PQ;
 
     frame_rate_supported = (((operating_rate >> 16) > 0) && ((operating_rate >> 16) < 5)) ? false : frame_rate_supported;
 
