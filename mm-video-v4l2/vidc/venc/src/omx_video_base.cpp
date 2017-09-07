6f6df14cfdee1d6c05577ac46628119915d3b114
Lajos Molnar <lajos@google.com>
2017-09-06 18:19:48 -0700

    mm-video-v4l2: venc: Advertise constrained profiles for AVC encoder
    Enumerate and advertise constrained profiles for AVC encoder.
    Inorder to have backward compatability advertise exisisting as well
    as newly added constants.
    
    Keep legacy constants for getters as Android media framework does not
    use them.
    
    Bug: 65043406
    
    Change-Id: I6fe88a505005731c4891aa1a7c1f627c65f01861

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -4354,6 +4354,9 @@ OMX_ERRORTYPE omx_video::get_supported_profile_level(OMX_VIDEO_PARAM_PROFILELEVE
                 profileLevelType->eProfile = OMX_VIDEO_AVCProfileHigh;
                 profileLevelType->eLevel   = OMX_VIDEO_AVCLevel52;
             } else if (profileLevelType->nProfileIndex == 3) {
+                profileLevelType->eProfile = OMX_VIDEO_AVCProfileConstrainedBaseline;
+                profileLevelType->eLevel   = OMX_VIDEO_AVCLevel52;
+            } else if (profileLevelType->nProfileIndex == 4) {
                 profileLevelType->eProfile = QOMX_VIDEO_AVCProfileConstrainedBaseline;
                 profileLevelType->eLevel   = OMX_VIDEO_AVCLevel52;
             } else {
@@ -4374,6 +4377,9 @@ OMX_ERRORTYPE omx_video::get_supported_profile_level(OMX_VIDEO_PARAM_PROFILELEVE
                 profileLevelType->eLevel   = OMX_VIDEO_AVCLevel4;
 #ifdef _MSM8226_
             } else if (profileLevelType->nProfileIndex == 3) {
+                profileLevelType->eProfile = OMX_VIDEO_AVCProfileConstrainedBaseline;
+                profileLevelType->eLevel   = OMX_VIDEO_AVCLevel4;
+            } else if (profileLevelType->nProfileIndex == 4) {
                 profileLevelType->eProfile = QOMX_VIDEO_AVCProfileConstrainedBaseline;
                 profileLevelType->eLevel   = OMX_VIDEO_AVCLevel4;
 #endif
