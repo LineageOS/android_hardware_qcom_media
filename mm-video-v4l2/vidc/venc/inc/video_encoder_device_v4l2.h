9edffd91101fb1cb4ad00f0d44aa6a85b0d50ea3
Umesh Pandey <umeshp@codeaurora.org>
2017-10-13 12:15:31 -0700

    mm-video-v4l2: vidc: Enable HEVC HDR10 PQ SEI
    Set controls to enable MASTERING_DISPLAY_INFO, CONTENT_LIGHT_INFO
    for HEVC MAIN10 recording. Also advertising only Main10HDR10 profile
    instead of Main10 profile.
    
    CRs-Fixed: 2126594
    Change-Id: I9d041192080c0e63b4b71084fcbba9c159d25694

--- mm-video-v4l2/vidc/venc/inc/video_encoder_device_v4l2.h
+++ mm-video-v4l2/vidc/venc/inc/video_encoder_device_v4l2.h
@@ -47,6 +47,7 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <media/msm_vidc.h>
 #include <poll.h>
 #include <list>
+#include "color_metadata.h"
 
 #define TIMEOUT 5*60*1000
 #define BIT(num) (1 << (num))
@@ -584,6 +585,7 @@ class venc_dev
         };
         BatchInfo mBatchInfo;
         bool mUseAVTimerTimestamps;
+        bool venc_set_hdr_info(const MasteringDisplay&, const ContentLightLevel&);
 };
 
 enum instance_state {
