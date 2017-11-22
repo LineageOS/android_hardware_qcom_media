5c54784f56912b5122dd9f7b1a2f4be1662ecfda
Surajit Podder <spodder@codeaurora.org>
2017-11-22 15:52:31 +0530

    mm-video-v4l2: Restore support of perf hint
    Restore support for perf hint from video HAL.
    
    Change-Id: Iedc92891251277c83208703ca697e6231165840c

--- mm-video-v4l2/vidc/venc/inc/omx_video_encoder.h
+++ mm-video-v4l2/vidc/venc/inc/omx_video_encoder.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2010-2017, The Linux Foundation. All rights reserved.
+Copyright (c) 2010-2018, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -98,6 +98,21 @@ class omx_venc: public omx_video
                                    int buffer_len, uint64_t timestamp);
         int dev_extradata_log_buffers(char *buffer);
         bool dev_get_hevc_profile(OMX_U32* profile);
+        class perf_control {
+            typedef int (*perf_lock_acquire_t)(int, int, int*, int);
+            typedef int (*perf_lock_release_t)(int);
+            public:
+                perf_control();
+                ~perf_control();
+                void send_hint_to_mpctl(bool state);
+            private:
+                int m_perf_handle;
+                void *m_perf_lib;
+                bool load_lib();
+                perf_lock_acquire_t m_perf_lock_acquire;
+                perf_lock_release_t m_perf_lock_release;
+        };
+        perf_control m_perf_control;
 };
 
 #ifdef _UBWC_
