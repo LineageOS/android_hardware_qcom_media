6f661313c54ff964310207f2f2ed9554781484f3
Mahesh Lanka <mlanka@codeaurora.org>
2017-12-05 14:16:36 +0530

    mm-core: Move Muxer component below dummy component.
    Prevent exposing internal muxer component to the VTS tests
    Put Muxer component below the dummy component
    entry in the registry file.
    
    Change-Id: I28dee36def078b00831b6233ec2abcdb503e0854

--- mm-core/src/8909/registry_table_android.c
+++ mm-core/src/8909/registry_table_android.c
@@ -411,19 +411,6 @@ omx_core_cb_type core[] =
     }
   },
   {
-    "OMX.qcom.file.muxer",
-    NULL, // Create instance function
-    // Unique instance handle
-    {
-      NULL
-    },
-    NULL,   // Shared object library handle
-    "libOmxMux.so",
-    {
-      "container_muxer.mp2"
-    }
-  },
-  {
     "OMX.qti.audio.decoder.alac.sw",
     NULL,   // Create instance function
     // Unique instance handle
@@ -527,6 +514,19 @@ omx_core_cb_type core[] =
     NULL,   // Shared object library handle
     {
       NULL
+    }
+  },
+  {
+    "OMX.qcom.file.muxer",
+    NULL, // Create instance function
+    // Unique instance handle
+    {
+      NULL
+    },
+    NULL,   // Shared object library handle
+    "libOmxMux.so",
+    {
+      "container_muxer.mp2"
     }
   },
   {
