0d18e515a34df222ce3c196f3c10b4f1238dc6ca
Chih-Hung Hsieh <chh@google.com>
2017-12-25 00:47:05 -0800

    Add -Wno-error to compile with global -Werror.
    Bug: 66996870
    Test: normal builds.
    Change-Id: Ie9d18e6ea8db12b0ccde25f2718d0ef83fe4ba3a

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -122,6 +122,8 @@ LOCAL_SRC_FILES         += src/omx_vdec_msm8974.cpp
 
 LOCAL_CLANG_CFLAGS      += -Wno-absolute-value -Wno-pointer-bool-conversion
 
+LOCAL_CFLAGS            += -Wno-error
+
 include $(BUILD_SHARED_LIBRARY)
 
 
@@ -163,6 +165,8 @@ endif
 LOCAL_SRC_FILES         += src/hevc_utils.cpp
 
 LOCAL_STATIC_LIBRARIES  := libOmxVidcCommon
+
+LOCAL_CFLAGS            += -Wno-error
 
 include $(BUILD_SHARED_LIBRARY)
 
