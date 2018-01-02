bdaa81b08cfa53fbfa3dcd56193b17560b279df6
Chih-Hung Hsieh <chh@google.com>
2018-01-02 11:34:10 -0800

    Add -Wno-error to compile with global -Werror.
    Bug: 66996870
    Test: normal builds.
    Change-Id: Iee4ac5f95a2013c5eeac1c061889a593caf0ec36

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -149,6 +149,7 @@ LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
 LOCAL_VENDOR_MODULE             := true
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    += -Wno-error
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 
 LOCAL_PRELINK_MODULE    := false
