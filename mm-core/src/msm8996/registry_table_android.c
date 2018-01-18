81153085eb18120ff26e004d19427005603c8da2
Paras Nagda <pnagda@codeaurora.org>
2018-01-18 14:12:29 +0530

    media - Changes for DIVX playback on O-MR1.
    Change in the MediaCodecxmlparser logic fails to recognize
    divx formats like divx4, divx311 on O MR1.Hence this change
    is required for playback of above formats
    
    Change-Id: I209a94e83fcb6c5163aef9453220b8a894b40ddd

--- mm-core/src/msm8996/registry_table_android.c
+++ mm-core/src/msm8996/registry_table_android.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
+Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -84,7 +84,11 @@ omx_core_cb_type core[] =
     NULL,   // Shared object library handle
     "libOmxVdec.so",
     {
+#ifdef _ANDROID_O_MR1_DIVX_CHANGES
+      "video_decoder.divx4"
+#else
       "video_decoder.divx"
+#endif
     }
   },
   {
@@ -116,7 +120,11 @@ omx_core_cb_type core[] =
     NULL,   // Shared object library handle
     "libOmxVdec.so",
     {
+#ifdef _ANDROID_O_MR1_DIVX_CHANGES
+      "video_decoder.divx311"
+#else
       "video_decoder.divx"
+#endif
     }
   },
   {
