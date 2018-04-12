e453f8322e231f26c8e3b4f8e29212dfe12b5549
SaiVamshi Dobbali <sdobbali@codeaurora.org>
2018-04-12 12:13:35 +0530

    mm-video-v4l2: Initialize clip to progressive by default.
    This issue is caused by commit:2714440a51b790dee65c4aa92a8b55dc883fac99.
    where in support for NV12 output for interlaced clip was added.
    
    Initialize clip as progressive by default before enabling C2D.
    
    Change-Id: Id52c7390d58d968ff6a069a6c80217dfb3f2d338

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -2368,6 +2368,8 @@ OMX_ERRORTYPE omx_vdec::component_init(OMX_STRING role)
         eRet = OMX_ErrorInvalidComponentName;
     }
 
+    m_progressive = MSM_VIDC_PIC_STRUCT_PROGRESSIVE;
+
     if (eRet == OMX_ErrorNone) {
         OMX_COLOR_FORMATTYPE dest_color_format;
         if (m_disable_ubwc_mode) {
@@ -2387,7 +2389,6 @@ OMX_ERRORTYPE omx_vdec::component_init(OMX_STRING role)
         }
 
         dpb_bit_depth = MSM_VIDC_BIT_DEPTH_8;
-        m_progressive = MSM_VIDC_PIC_STRUCT_PROGRESSIVE;
         is_flexible_format = FALSE;
         is_mbaff = FALSE;
 
