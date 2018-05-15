8932f5c1e18edf43d8e7c737ba28dbd521c47256
Santhosh Behara <santhoshbehara@codeaurora.org>
2018-05-15 12:06:52 +0530

    media: Fix copyright update
    Fix copyright update
    
    Change-Id: Iee25e8e97855f2c5ca75f25c9fb8b914010514f3

--- mm-core/inc/OMX_Video.h
+++ mm-core/inc/OMX_Video.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008, 2018 The Khronos Group Inc.
+ * Copyright (c) 2008 The Khronos Group Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
