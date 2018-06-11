d66fdb7c1c0a5cc5c1bcc394e7ffb171fb3d2d31
Karthikeyan Periasamy <kperiasa@codeaurora.org>
2018-06-11 15:10:45 -0700

    mm-video-v4l2: Support NV12 o/p format for interlaced clips
    * Initialize clip to progressive by default.
    * Allocate intermediate output buffers same as output buffers
      if color conversion is enabled.
    * Send intermediate buffers to driver for decoded buffers
    * Send intermediate decoded buffers to color conversion, update
      client buffers with color conversion output and send them to client.
    
    CRs-Fixed: 2258387
    Change-Id: Ifeb588ae39a023f64f0a3163dd78b57ae03abaf0

--- mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
+++ mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
@@ -469,9 +469,12 @@ struct video_driver_context {
     struct vdec_bufferpayload *ptr_inputbuffer;
     struct vdec_bufferpayload *ptr_outputbuffer;
     struct vdec_output_frameinfo *ptr_respbuffer;
+    struct vdec_bufferpayload *ptr_intermediate_outputbuffer;
+    struct vdec_output_frameinfo *ptr_intermediate_respbuffer;
 #ifdef USE_ION
     struct vdec_ion *ip_buf_ion_info;
     struct vdec_ion *op_buf_ion_info;
+    struct vdec_ion *op_intermediate_buf_ion_info;
     struct vdec_ion h264_mv;
     struct vdec_ion meta_buffer;
     struct vdec_ion meta_buffer_iommu;
@@ -854,8 +857,9 @@ class omx_vdec: public qc_omx_component
         OMX_ERRORTYPE free_input_buffer(OMX_BUFFERHEADERTYPE *bufferHdr);
         OMX_ERRORTYPE free_input_buffer(unsigned int bufferindex,
                 OMX_BUFFERHEADERTYPE *pmem_bufferHdr);
-        OMX_ERRORTYPE free_output_buffer(OMX_BUFFERHEADERTYPE *bufferHdr);
-        void free_output_buffer_header();
+        OMX_ERRORTYPE free_output_buffer(OMX_BUFFERHEADERTYPE *bufferHdr,
+                                         bool                 intermediate=false);
+        void free_output_buffer_header(bool intermediate=false);
         void free_input_buffer_header();
         void free_output_extradata_buffer_header();
 
@@ -868,7 +872,9 @@ class omx_vdec: public qc_omx_component
         OMX_ERRORTYPE allocate_output_buffer(OMX_HANDLETYPE       hComp,
                 OMX_BUFFERHEADERTYPE **bufferHdr,
                 OMX_U32 port,OMX_PTR appData,
-                OMX_U32              bytes);
+                OMX_U32              bytes,
+                bool                 intermediate=false,
+                int                  index=-1);
         OMX_ERRORTYPE use_output_buffer(OMX_HANDLETYPE hComp,
                 OMX_BUFFERHEADERTYPE   **bufferHdr,
                 OMX_U32                port,
@@ -884,7 +890,7 @@ class omx_vdec: public qc_omx_component
         OMX_ERRORTYPE get_supported_profile_level(OMX_VIDEO_PARAM_PROFILELEVELTYPE *profileLevelType);
 
         OMX_ERRORTYPE allocate_desc_buffer(OMX_U32 index);
-        OMX_ERRORTYPE allocate_output_headers();
+        OMX_ERRORTYPE allocate_output_headers(bool intermediate=false);
         OMX_ERRORTYPE allocate_client_output_extradata_headers();
         bool execute_omx_flush(OMX_U32);
         bool execute_output_flush();
@@ -1073,6 +1079,8 @@ class omx_vdec: public qc_omx_component
         OMX_BUFFERHEADERTYPE  *m_inp_mem_ptr;
         // Output memory pointer
         OMX_BUFFERHEADERTYPE  *m_out_mem_ptr;
+        // Output memory pointer
+        OMX_BUFFERHEADERTYPE  *m_intermediate_out_mem_ptr;
         // Client extradata memory pointer
         OMX_BUFFERHEADERTYPE  *m_client_output_extradata_mem_ptr;
         // number of input bitstream error frame count
@@ -1259,7 +1267,6 @@ class omx_vdec: public qc_omx_component
                 OMX_ERRORTYPE set_buffer_req(OMX_U32 buffer_size, OMX_U32 actual_count);
                 OMX_BUFFERHEADERTYPE* get_il_buf_hdr();
                 OMX_BUFFERHEADERTYPE* get_il_buf_hdr(OMX_BUFFERHEADERTYPE *input_hdr);
-                OMX_BUFFERHEADERTYPE* get_dr_buf_hdr(OMX_BUFFERHEADERTYPE *input_hdr);
                 OMX_BUFFERHEADERTYPE* convert(OMX_BUFFERHEADERTYPE *header);
                 OMX_BUFFERHEADERTYPE* queue_buffer(OMX_BUFFERHEADERTYPE *header);
                 OMX_ERRORTYPE allocate_buffers_color_convert(OMX_HANDLETYPE hComp,
