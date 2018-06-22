564f6583d9b1f3270d806130a523a0da74356871
Maheshwar Ajja <majja@codeaurora.org>
2018-06-21 17:02:17 -0700

    mm-video-v4l2: venc: Increase buffer count for HFR usecase
    High Frame Rate (HFR) usecase requires more input and output
    buffers so increase the buffer count for HFR usecase.
    
    Change-Id: Ia15961cbb76c2f6dd35b78a175fc6d60a8a310c5

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -4136,7 +4136,7 @@ OMX_ERRORTYPE  omx_video::empty_this_buffer_proxy(OMX_IN OMX_HANDLETYPE  hComp,
             }
             Input_pmem_info.offset = offset;
             Input_pmem_info.size = size;
-            DEBUG_PRINT_INFO("ETB (meta-Camera) fd = %d, offset = %d, size = %d",
+            DEBUG_PRINT_HIGH("ETB (meta-Camera) fd = %d, offset = %d, size = %d",
                     Input_pmem_info.fd, Input_pmem_info.offset,
                     Input_pmem_info.size);
         } else {
