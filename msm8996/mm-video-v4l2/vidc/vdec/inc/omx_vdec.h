f42b03df24d3acd5caaa2628c206c591ccc023eb
Ray Essick <essick@google.com>
2018-07-02 16:00:09 -0700

    mm-video-v4l2: Protect buffer access and increase input buffer size
    Protect buffer access for below scenarios:
    
    *Increase the scope of buf_lock in free_buffer to avoid access
     of freed buffer for both input and output buffers. Also, add check
     before output buffer access.
    
    *Disallow allocate buffer mode after client has called use buffer.
    
    Allocate additional 512 bytes of memory for input buffers on top of
    allocation size as per hardware requirement.
    
    Bug: 64340487
    Test: ran POC on sailfish/nyc-mr1-dev
    Change-Id: Ic13b511b2d1c46e0b158880c41c9a02e2fefd113
    Merged-In: Id699404d6c276c8702826d79b3a6ba9755b3439f
    (cherry picked from commit 660db9e9708872f9a407ac14e92a5017599ced34)

--- msm8996/mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
+++ msm8996/mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2010 - 2016, The Linux Foundation. All rights reserved.
+Copyright (c) 2010 - 2016, 2018, The Linux Foundation. All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions
@@ -1173,6 +1173,7 @@ class omx_vdec: public qc_omx_component
         static OMX_ERRORTYPE describeColorFormat(OMX_PTR params);
         void prefetchNewBuffers();
 
+        bool m_buffer_error;
 };
 
 #ifdef _MSM8974_
