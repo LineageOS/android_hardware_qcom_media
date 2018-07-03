bb3bafff4012b6e1ebebb34c16e5074e3c263a40
Indranil <ichakrab@codeaurora.org>
2018-07-03 14:33:35 +0530

    Revert "libaac: Add test code for AAC encoder and decoder"
    This reverts commit 1b463445c750f65f62f9d5937679fc69455460f6.
    
    Change is now part of qcom-opensource/wfd-commonsys project.
    
    Change-Id: Ib392ea4eabb6f7216b59f5385a514c56593871b7

--- Android.mk
+++ Android.mk
@@ -6,7 +6,6 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-include $(QCOM_MEDIA_ROOT)/libaac/Android.mk
 endif
 
 ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
