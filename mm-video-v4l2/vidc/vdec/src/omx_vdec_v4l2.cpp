46d2425f2b2a5c2d9dd7a726758f75fd11bb44e4
Vaibhav Deshu Venkatesh <vdeshuve@codeaurora.org>
2018-07-25 17:55:45 -0700

    mm-video-v4l2: vdec: Fix extradata buffer free in reconfig
    During reconfig extra buffer is being freed with updated
    buffer size instead of size with which it was allocated.
    Use previous size correctly to free it.
    
    CRs-Fixed: 2280521
    Change-Id: I5b0ddf0577c8857c0f907514358424d7f355cfd5

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -5732,10 +5732,7 @@ OMX_ERRORTYPE omx_vdec::allocate_extradata()
 #ifdef USE_ION
     if (drv_ctx.extradata_info.buffer_size) {
         if (drv_ctx.extradata_info.ion.data_fd >= 0) {
-            ion_unmap(drv_ctx.extradata_info.ion.data_fd,
-                      (void *)drv_ctx.extradata_info.uaddr,
-                      drv_ctx.extradata_info.size);
-            free_ion_memory(&drv_ctx.extradata_info.ion);
+            free_extradata();
         }
 
         drv_ctx.extradata_info.size = (drv_ctx.extradata_info.size + 4095) & (~4095);
@@ -5766,8 +5763,10 @@ void omx_vdec::free_extradata()
     if (drv_ctx.extradata_info.uaddr) {
         ion_unmap(drv_ctx.extradata_info.ion.data_fd,
                   (void *)drv_ctx.extradata_info.uaddr,
-                  drv_ctx.extradata_info.size);
+                  drv_ctx.extradata_info.ion.alloc_data.len);
         free_ion_memory(&drv_ctx.extradata_info.ion);
+        drv_ctx.extradata_info.uaddr = NULL;
+        drv_ctx.extradata_info.ion.data_fd = -1;
     }
 #endif
 }
