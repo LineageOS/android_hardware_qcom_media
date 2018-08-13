3b021893bd9ea237c17a3849633cdb28bf1d19ee
Tapas Kumar Kundu <tkundu@codeaurora.org>
2018-08-13 15:22:03 -0700

    mm-video: Print v4l2 color format and gralloc buffer format in logcat
    This change prints v4l2 color format and gralloc buffer format in
    logcat.
    
    CRs-Fixed: 2296074
    Change-Id: Idb8567f3f853c0e57e58bea627073f4e0db22469

--- mm-video-v4l2/vidc/common/inc/vidc_common.h
+++ mm-video-v4l2/vidc/common/inc/vidc_common.h
@@ -26,7 +26,15 @@ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --------------------------------------------------------------------------*/
+#ifndef __VIDC_COMMON_H__
+#define __VIDC_COMMON_H__
+
 #include <unordered_map>
+
+#include "OMX_QCOMExtns.h"
+#ifdef _ANDROID_
+#include <gralloc_priv.h>
+#endif
 
 using pl_map = std::unordered_map<int, int>;
 using codec_map = std::unordered_map<int, pl_map *>;
@@ -72,3 +80,8 @@ class profile_level_converter {
     static bool convert_v4l2_level_to_omx(int codec, int v4l2_level, int *omx_level);
     static bool convert_omx_level_to_v4l2(int codec, int omx_level, int *v4l2_level);
 };
+
+void get_gralloc_format_as_string(char * buf, int buf_len, int format);
+void get_v4l2_color_format_as_string(char * buf, int buf_len, unsigned long v4l2Pixformat);
+
+#endif
