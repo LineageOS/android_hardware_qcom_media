629adbabb3d63f21d8b3bfd7a78e790c562649d8
Amit Shekhar <ashekhar@codeaurora.org>
2018-08-31 14:54:47 -0700

    media: Fix color format for HW HEIC encode
    Fix get/set color format for HW HEIC encode. Color format for HW HEIC
    encode is V4L2_PIX_FMT_NV12_512.
    
    Change-Id: I8bb2824bb22f5d8173c8c5054c3cb2d2ed0d789b

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -1452,6 +1452,9 @@ int venc_dev::venc_input_log_buffers(OMX_BUFFERHEADERTYPE *pbuffer, int fd, int 
             case V4L2_PIX_FMT_NV12:
                 color_format = COLOR_FMT_NV12;
                 break;
+            case V4L2_PIX_FMT_NV12_512:
+                color_format = COLOR_FMT_NV12_512;
+                break;
             case V4L2_PIX_FMT_NV12_UBWC:
                 color_format = COLOR_FMT_NV12_UBWC;
                 break;
@@ -2451,7 +2454,7 @@ bool venc_dev::venc_set_param(void *paramData, OMX_INDEXTYPE index)
                 }
 
                 if (!venc_set_grid_enable()) {
-                    DEBUG_PRINT_ERROR("ERROR: Failed to set tile dimension");
+                    DEBUG_PRINT_ERROR("ERROR: Failed to set grid-enable");
                     return false;
                 }
                 break;
@@ -5224,7 +5227,7 @@ bool venc_dev::venc_set_grid_enable()
     int rc;
     struct v4l2_control control;
 
-    DEBUG_PRINT_LOW("venc_set_tile_dimension");
+    DEBUG_PRINT_LOW("venc_set_grid_enable");
     control.id = V4L2_CID_MPEG_VIDC_IMG_GRID_ENABLE;
     control.value = 1;
     rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
@@ -5719,6 +5722,9 @@ unsigned long venc_dev::venc_get_color_format(OMX_COLOR_FORMATTYPE eColorFormat)
         break;
     }
 
+    if (m_codec == OMX_VIDEO_CodingImageHEIC)
+        format = V4L2_PIX_FMT_NV12_512;
+
     return format;
 }
 
@@ -5761,6 +5767,9 @@ bool venc_dev::venc_set_color_format(OMX_COLOR_FORMATTYPE color_format)
             DEBUG_PRINT_HIGH("Default color format NV12 UBWC is set");
             break;
     }
+
+    if (m_codec == OMX_VIDEO_CodingImageHEIC)
+        m_sVenc_cfg.inputformat = V4L2_PIX_FMT_NV12_512;
 
     memset(&fmt, 0, sizeof(fmt));
     fmt.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
