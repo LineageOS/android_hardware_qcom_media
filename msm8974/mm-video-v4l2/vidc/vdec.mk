621eef4f29aeb5a969808d6a2cf10e31a823ade8
razorloves <razorloves@gmail.com>
2018-10-03 12:39:23 -0500

    media: Use generated kernel headers
    Change-Id: I8d7184523c5e24a43f38efb8192c50186462991f

--- msm8974/mm-video-v4l2/vidc/vdec.mk
+++ msm8974/mm-video-v4l2/vidc/vdec.mk
@@ -114,6 +114,8 @@ LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 
+LOCAL_HEADER_LIBRARIES  := generated_kernel_headers
+
 LOCAL_SHARED_LIBRARIES  := liblog libutils libui libbinder libcutils libdl
 
 LOCAL_SHARED_LIBRARIES  += libdivxdrmdecrypt
@@ -158,6 +160,8 @@ LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 
+LOCAL_HEADER_LIBRARIES  := generated_kernel_headers
+
 LOCAL_SHARED_LIBRARIES  := liblog libutils libui libbinder libcutils libdl
 
 LOCAL_SHARED_LIBRARIES  += libdivxdrmdecrypt
@@ -194,6 +198,8 @@ LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
 LOCAL_C_INCLUDES                := $(mm-vdec-test-inc)
 
+LOCAL_HEADER_LIBRARIES    := generated_kernel_headers
+
 LOCAL_SHARED_LIBRARIES    := libutils libui libOmxCore libOmxVdec libbinder libcutils
 
 LOCAL_SRC_FILES           := vdec/src/queue.c
@@ -214,6 +220,8 @@ LOCAL_MODULE                    := mm-video-driver-test
 LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
 LOCAL_C_INCLUDES                := $(mm-vdec-drv-test-inc)
+
+LOCAL_HEADER_LIBRARIES          := generated_kernel_headers
 
 LOCAL_SRC_FILES                 := vdec/src/message_queue.c
 LOCAL_SRC_FILES                 += vdec/test/decoder_driver_test.c
