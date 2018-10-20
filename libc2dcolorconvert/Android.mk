afe36570d1e7d63574685f45b0b2f70e841412a8
Arne Coucheron <arco68@gmail.com>
2018-10-21 00:27:58 +0200

    media: Use generated kernel headers
    Change-Id: I2f848353e2e8d1ba0b233c1d0b271e0595ff9a79

--- libc2dcolorconvert/Android.mk
+++ libc2dcolorconvert/Android.mk
@@ -9,13 +9,13 @@ LOCAL_C_INCLUDES := \
     $(TOP)/frameworks/av/include/media/stagefright \
     $(TOP)/frameworks/native/include/media/openmax \
     $(TOP)/$(call project-path-for,qcom-display)/libcopybit
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_SHARED_LIBRARIES := liblog libdl
+
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := libc2dcolorconvert
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_VENDOR_MODULE := true
 
