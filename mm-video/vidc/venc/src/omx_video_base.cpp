AgentFabulous/android_oreo_bacon

mm-video-v4l2: vidc: Handle native_handle meta mode
 - Meta mode for sending native handles from camera
   should be supported along with rest of the modes.

BUG: 34516149
Test: Camera CTS API1 and API2 using legacy Hal1 module
Change-Id: Ia9dc5936b0b5a2792b1286b979030f0f5b6104a1

msm8974/mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -3454,10 +3454,12 @@ OMX_ERRORTYPE  omx_video::empty_this_buffer_proxy(OMX_IN OMX_HANDLETYPE  hComp,
         media_buffer = (encoder_media_buffer_type *)meta_buffer_hdr[nBufIndex].pBuffer;
         if (media_buffer) {
             if (media_buffer->buffer_type != kMetadataBufferTypeCameraSource &&
-                    media_buffer->buffer_type != kMetadataBufferTypeGrallocSource) {
+                    media_buffer->buffer_type != kMetadataBufferTypeGrallocSource &&
+                    media_buffer->buffer_type != kMetadataBufferTypeNativeHandleSource) {
                 met_error = true;
             } else {
-                if (media_buffer->buffer_type == kMetadataBufferTypeCameraSource) {
+                if ((media_buffer->buffer_type == kMetadataBufferTypeCameraSource) ||
+                        (media_buffer->buffer_type == kMetadataBufferTypeNativeHandleSource)) {
                     if (media_buffer->meta_handle == NULL)
                         met_error = true;
                     else if ((media_buffer->meta_handle->numFds != 1 &&
@@ -4474,7 +4476,8 @@ void omx_video::omx_release_meta_buffer(OMX_BUFFERHEADERTYPE *buffer)
         } else {
             media_ptr = (encoder_media_buffer_type *) buffer->pBuffer;
             if (media_ptr && media_ptr->meta_handle) {
-                if (media_ptr->buffer_type == kMetadataBufferTypeCameraSource &&
+                if ((media_ptr->buffer_type == kMetadataBufferTypeCameraSource ||
+                        media_ptr->buffer_type == kMetadataBufferTypeNativeHandleSource) &&
                         media_ptr->meta_handle->numFds == 1 &&
                         media_ptr->meta_handle->numInts >= 2) {
                     Input_pmem.fd = media_ptr->meta_handle->data[0];

LineageOS/android_hardware_qcom_media

mm-video/vidc/venc/src/omx_video_base.cpp
@@ -3526,7 +3526,8 @@ OMX_ERRORTYPE  omx_video::empty_this_buffer_proxy(OMX_IN OMX_HANDLETYPE         
         met_error = true;
     } else if (media_buffer) {
         if (media_buffer->buffer_type != LEGACY_CAM_SOURCE &&
-          media_buffer->buffer_type != kMetadataBufferTypeGrallocSource) {
+          media_buffer->buffer_type != kMetadataBufferTypeGrallocSource &&
+          media_buffer->buffer_type != kMetadataBufferTypeNativeHandleSource) {
           met_error = true;
       } else {
         if(media_buffer->buffer_type == LEGACY_CAM_SOURCE)
@@ -4572,7 +4573,8 @@ void omx_video::omx_release_meta_buffer(OMX_BUFFERHEADERTYPE *buffer)
       media_ptr = (LEGACY_CAM_METADATA_TYPE *) buffer->pBuffer;
       if(media_ptr && media_ptr->meta_handle)
       {
-        if(media_ptr->buffer_type == LEGACY_CAM_SOURCE &&
+        if((media_ptr->buffer_type == LEGACY_CAM_SOURCE ||
+           media_ptr->buffer_type == kMetadataBufferTypeNativeHandleSource) &&
            media_ptr->meta_handle->numFds == 1 &&
            media_ptr->meta_handle->numInts == 2) {
           Input_pmem.fd = media_ptr->meta_handle->data[0];