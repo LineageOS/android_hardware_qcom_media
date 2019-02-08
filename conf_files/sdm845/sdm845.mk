08ddb4d8e1daa7b514528b3211c218d79fc73da0
Uma Mehta <umamehta@codeaurora.org>
2019-02-08 10:41:20 +0530

    media: Add config and system properties for sdm845
    - Move config xmls and system properties to
      corresponding video component.
    
    CRs-Fixed: 2394050
    
    Change-Id: I2de4ba05691270440476072f99f625a21d0e6b37

--- conf_files/sdm845/sdm845.mk
+++ conf_files/sdm845/sdm845.mk
@@ -1,1 +1,15 @@
-#Bring all FEATURE FLAG (Compilation) here
+CONFIG_PATH := hardware/qcom/media/conf_files/sdm845
+
+# Video feature flags
+
+# Video configuration files
+PRODUCT_COPY_FILES += \
+    $(CONFIG_PATH)/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(CONFIG_PATH)/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(CONFIG_PATH)/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
+    $(CONFIG_PATH)/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
+    $(CONFIG_PATH)/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
+    $(CONFIG_PATH)/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(CONFIG_PATH)/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
+
+# Vendor property overrides
