4180c26418f2f6a246fc964926906263d85acec6
Vaibhav Deshu Venkatesh <vdeshuve@codeaurora.org>
2019-02-21 12:47:14 -0800

    mm-video-v4l2: venc: Remove get control from NAL setting
    While setting NAL configuration there is a get control to
    check if the current NAL setting is valid. This check is
    not needed any longer hence remove it.
    
    CRs-Fixed: 2354339
    Change-Id: I220ce0d6159a1026acb414b0e883a9c724811108

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -5089,40 +5089,28 @@ bool venc_dev::venc_get_profile_level(OMX_U32 *eProfile,OMX_U32 *eLevel)
 }
 
 bool venc_dev::venc_set_nal_size (OMX_VIDEO_CONFIG_NALSIZE *nalSizeInfo) {
-    struct v4l2_control gControl;
     struct v4l2_control sControl;
 
     DEBUG_PRINT_HIGH("set video stream format - nal size - %u", nalSizeInfo->nNaluBytes);
-    gControl.id = V4L2_CID_MPEG_VIDEO_HEVC_SIZE_OF_LENGTH_FIELD;
-
-    if (ioctl(m_nDriver_fd, VIDIOC_G_CTRL, &gControl)) {
-        DEBUG_PRINT_ERROR("get control: video stream format failed");
-        return false;
-    }
 
     sControl.id = V4L2_CID_MPEG_VIDEO_HEVC_SIZE_OF_LENGTH_FIELD;
     switch (nalSizeInfo->nNaluBytes) {
-            case 0:
-                sControl.value = V4L2_MPEG_VIDEO_HEVC_SIZE_0;
-                break;
-            case 4:
-                sControl.value = V4L2_MPEG_VIDEO_HEVC_SIZE_4;
-                break;
-            default:
-                return false;
-        }
-
-    if ((1 << sControl.value) & gControl.value) {
-        if (ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &sControl)) {
-            DEBUG_PRINT_ERROR("set control: video stream format failed - %u",
-                    (unsigned int)nalSizeInfo->nNaluBytes);
+        case 0:
+            sControl.value = V4L2_MPEG_VIDEO_HEVC_SIZE_0;
+            break;
+        case 4:
+            sControl.value = V4L2_MPEG_VIDEO_HEVC_SIZE_4;
+            break;
+        default:
             return false;
-        }
-        return true;
     }
 
-    return false;
-
+    if (ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &sControl)) {
+        DEBUG_PRINT_ERROR("set control: video stream format failed - %u",
+                (unsigned int)nalSizeInfo->nNaluBytes);
+        return false;
+    }
+    return true;
 }
 
 bool venc_dev::venc_store_dynamic_config(OMX_INDEXTYPE config_type, OMX_PTR config)
