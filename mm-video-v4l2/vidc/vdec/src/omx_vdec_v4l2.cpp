45cb937703c3f7135104d9ad3bc3f4f281f0b73f
Govindaraj Rajagopal <grajagop@codeaurora.org>
2019-02-22 18:14:57 +0530

    mm-video-v4l2: vdec: Avoid race condition for unsupported clip.
    when the decoder state moves from idle->executing, ETB and
    FTB starts getting queued to the decoder.For an unsupported
    clip, once the user issues flush while ETB are being fed and
    at the same time there is an error detected from the hardware,
    there is a race condition happening to acquire m_lock by
    post_event() and the thread which has already acquired same
    m_lock in function send_codec_config().
    Fix here is to release and regain the m_lock before and after
    call being made to empty_this_buffer_proxy() inside the function
    send_codec_config(), so that race condition does not occur for
    this scenerio.
    
    Change-Id: I58288c1aab52466b73c79f42a8812be631b1b8e6
    Signed-off-by: Govindaraj Rajagopal <grajagop@codeaurora.org>

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -12709,22 +12709,26 @@ void omx_vdec::send_codec_config() {
             m_etb_q.pop_entry(&p1,&p2,&ident);
             if (ident == OMX_COMPONENT_GENERATE_ETB_ARBITRARY) {
                 if (((OMX_BUFFERHEADERTYPE *)p2)->nFlags & OMX_BUFFERFLAG_CODECCONFIG) {
+                    pthread_mutex_unlock(&m_lock);
                     if (empty_this_buffer_proxy_arbitrary((OMX_HANDLETYPE)p1,\
                                 (OMX_BUFFERHEADERTYPE *)p2) != OMX_ErrorNone) {
                         DEBUG_PRINT_ERROR("\n empty_this_buffer_proxy_arbitrary failure");
                         omx_report_error();
                     }
+                    pthread_mutex_lock(&m_lock);
                 } else {
                     DEBUG_PRINT_LOW("\n Flush Input Heap Buffer %p",(OMX_BUFFERHEADERTYPE *)p2);
                     m_cb.EmptyBufferDone(&m_cmp ,m_app_data, (OMX_BUFFERHEADERTYPE *)p2);
                 }
             } else if (ident == OMX_COMPONENT_GENERATE_ETB) {
                 if (((OMX_BUFFERHEADERTYPE *)p2)->nFlags & OMX_BUFFERFLAG_CODECCONFIG) {
+                    pthread_mutex_unlock(&m_lock);
                     if (empty_this_buffer_proxy((OMX_HANDLETYPE)p1,\
                                 (OMX_BUFFERHEADERTYPE *)p2) != OMX_ErrorNone) {
                         DEBUG_PRINT_ERROR("\n empty_this_buffer_proxy failure");
                         omx_report_error ();
                     }
+                    pthread_mutex_lock(&m_lock);
                 } else {
                     pending_input_buffers++;
                     VIDC_TRACE_INT_LOW("ETB-pending", pending_input_buffers);
