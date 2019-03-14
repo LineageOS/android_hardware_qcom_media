0d4dfc2958bbe358cbba3ba07ad4e404c9ff6c8b
Akshay Chandrashekhar Kalghatgi <akalghat@codeaurora.org>
2019-03-14 12:45:31 -0700

    mm-video-v4l2: vidc: vdec: Disable Prefetch
    Disabling prefetch temporarily till ION driver prefetch API
    changes are in place.
    
    Change-Id: Id33d071c595591840b10d3c534e87627790803ff

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -10409,6 +10409,10 @@ OMX_ERRORTYPE omx_vdec::describeColorFormat(OMX_PTR pParam)
 bool omx_vdec::prefetch_buffers(unsigned long prefetch_count,
         unsigned long prefetch_size, unsigned ioctl_code, unsigned ion_flag)
 {
+    int rc = false;
+    DEBUG_PRINT_ERROR("Prefetch is disabled! count %ld size %ld ioctl %#x flag %d",
+        prefetch_count, prefetch_size, ioctl_code, ion_flag);
+#if 0
     struct ion_prefetch_data prefetch_data;
     struct ion_prefetch_regions regions;
     __u64 sizes[prefetch_count];
@@ -10442,6 +10446,7 @@ bool omx_vdec::prefetch_buffers(unsigned long prefetch_count,
     }
 
     close(ion_fd);
+#endif
     return rc;
 }
 
