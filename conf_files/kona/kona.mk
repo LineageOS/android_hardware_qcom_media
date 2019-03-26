1616f0b7d00c11e00b06d1f41a084d54339cfd3b
Mihir Ganu <mganu@codeaurora.org>
2019-03-26 11:17:38 -0700

    conf_files: Add integer-overflow sanitization debug flag
    Add a debug flag to enable CLANG integer-overflow diag mode
    in video modules.
    
    CRs-Fixed: 2373430
    Change-Id: I99ca496b4927653fcd50a42ae8355336cf67610b

--- conf_files/kona/kona.mk
+++ conf_files/kona/kona.mk
@@ -10,4 +10,7 @@ PRODUCT_COPY_FILES += \
     $(CONFIG_PATH)/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(CONFIG_PATH)/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
+# Enable debug mode for CLANG/LLVM integer-overflow sanitization
+TARGET_ENABLE_VIDC_INTSAN_DIAG := true
+
 # Vendor property overrides
