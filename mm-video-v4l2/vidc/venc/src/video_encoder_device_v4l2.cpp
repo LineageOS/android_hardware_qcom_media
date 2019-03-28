dbf5b3f866a0893e9df680f2c552e6d6dcc8d5c6
Hangyu Kuang <hkuang@google.com>
2019-03-28 15:30:59 -0700

    Remove UBWC hardcode in qcom video driver.
    Bug: 129353499
    Test: Camera recording.
    Change-Id: Iffd91a5217a6c74e378ee5cc6b70a8e3dec7b21e

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -7236,9 +7236,6 @@ void venc_dev::venc_get_consumer_usage(OMX_U32* usage) {
     /* Initialize to zero & update as per required color format */
     *usage = 0;
 
-    /* Configure UBWC as default */
-    *usage |= GRALLOC_USAGE_PRIVATE_ALLOC_UBWC;
-
     if (hevc && eProfile == (OMX_U32)OMX_VIDEO_HEVCProfileMain10HDR10) {
         DEBUG_PRINT_INFO("Setting 10-bit consumer usage bits");
         *usage |= GRALLOC_USAGE_PRIVATE_10BIT_VIDEO;
