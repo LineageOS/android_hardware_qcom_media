166536d377555b4b807b7d07ad82e2c1154b7722
Hangyu Kuang <hkuang@google.com>
2019-03-28 15:30:59 -0700

    Remove UBWC hardcode in qcom video driver.
    Bug: 129353499
    Test: Camera recording.
    Change-Id: Iffd91a5217a6c74e378ee5cc6b70a8e3dec7b21e

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -38,6 +38,10 @@ endif
 
 libmm-venc-def += -D_UBWC_
 
+ifeq ($(TARGET_DISABLED_UBWC),true)
+libmm-venc-def += -DDISABLE_UBWC
+endif
+
 ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_SUPPORT_VQZIP)),true)
 libmm-venc-def += -D_VQZIP_
 endif
