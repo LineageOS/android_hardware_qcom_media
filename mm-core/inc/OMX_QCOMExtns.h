f89d1c652515a5addea29781d945b3d6bbd73ced
Qiwei Liu <qiweil@codeaurora.org>
2019-05-16 15:57:49 +0800

    mm-video-v4l2: support conditional compiling for extradata
    Add one more definition for extradata to support conditional
    compiling, as enums are not recognized at that stage.
    
    Change-Id: Ifb3b5e124ebc1d69f7fb3445026f883500bdb358

--- mm-core/inc/OMX_QCOMExtns.h
+++ mm-core/inc/OMX_QCOMExtns.h
@@ -1473,6 +1473,8 @@ typedef enum OMX_QTI_VIDC_EXTRADATATYPE
     OMX_QTI_VIDC_ExtraData_HDR10HIST =                  0x7F100008,
 } OMX_QTI_VIDC_EXTRADATATYPE;
 
+#define OMX_QTI_VIDC_ExtraData_LTRInfo OMX_QTI_VIDC_ExtraData_LTRInfo
+
 struct ExtraDataMap {
         const char *type;
         OMX_QTI_VIDC_EXTRADATATYPE index;
