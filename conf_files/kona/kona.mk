b8de254e568b01de06fa1fbf9788f6ede2aa9825
Arun Mirpuri <amirpuri@codeaurora.org>
2019-06-21 12:25:16 -0700

    media: Include QC OMX Codecs and fix rank of QC OMX components
    Include media_codecs_vendor_audio.xml from within media_codecs.xml
    and rank QC OMX SW codecs higher than C2 codecs.
    
    Change-Id: I9b0536cfb31265052c49a8634bdec9f99bb3e58e

--- conf_files/kona/kona.mk
+++ conf_files/kona/kona.mk
@@ -21,6 +21,7 @@ ifeq ($(GENERIC_ODM_IMAGE),true)
   PRODUCT_ODM_PROPERTIES += debug.stagefright.omx_default_rank=1000
 else
   $(warning "Enabling codec2.0 SW only for non-generic odm build variant")
-  #Rank SW C2 codecs first
-  PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=1
+  #Rank OMX SW codecs lower than OMX HW codecs
+  PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank.sw-audio=1
+  PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank=0
 endif
