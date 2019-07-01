fa91f3cb3fe4831098a71a4e326cb80572917e7b
Sujin Panicker <spanic@codeaurora.org>
2019-07-01 17:30:33 +0530

    media: fix rank of QC OMX components
    Rank OMX SW codecs higher than C2 codecs except for opus codec.
    
    Change-Id: I8d331e70d9e8edd4ee9a629e7cdeb27880c7beb0

--- conf_files/sdm710/sdm710.mk
+++ conf_files/sdm710/sdm710.mk
@@ -15,3 +15,14 @@ PRODUCT_COPY_FILES += \
     $(CONFIG_PATH)/../sdm845/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
 # Vendor property overrides
+ifeq ($(GENERIC_ODM_IMAGE),true)
+  $(warning "Forcing codec2.0 HW for generic odm build variant")
+  #Set default ranks and rank Codec 2.0 over OMX codecs
+  PRODUCT_ODM_PROPERTIES += debug.stagefright.ccodec=4
+  PRODUCT_ODM_PROPERTIES += debug.stagefright.omx_default_rank=1000
+else
+  $(warning "Enabling codec2.0 SW only for non-generic odm build variant")
+  #Rank OMX SW codecs lower than OMX HW codecs
+  PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank.sw-audio=1
+  PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank=0
+endif
