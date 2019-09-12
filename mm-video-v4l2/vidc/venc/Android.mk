51a0a1361f9d722e86afb53aeef27be4a5a982d2
Chippa-a <vusal1372@gmail.com>
2019-09-12 23:04:02 +0300

    Revert "Media: Disable CLANG for this module."
    Disabling CLANG is no longer supported starting
    from Android 10.
    
    This reverts commit d06c29b057a843e752332981c7f122eb55d43bd9.
    
    Change-Id: Ib17b531ecfed7e0ec4886754daaded564bcf84f9

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -83,7 +83,6 @@ LOCAL_MODULE                    := libOmxVenc
 LOCAL_MODULE_TAGS               := optional
 LOCAL_VENDOR_MODULE             := true
 LOCAL_CFLAGS                    := $(libmm-venc-def)
-LOCAL_CLANG := false
 LOCAL_C_INCLUDES                := $(libmm-venc-inc)
 
 LOCAL_PRELINK_MODULE      := false
@@ -132,7 +131,6 @@ LOCAL_STATIC_LIBRARIES    := libOmxVidcCommon
 
 LOCAL_SRC_FILES   := src/omx_video_base.cpp
 LOCAL_SRC_FILES   += src/omx_swvenc_mpeg4.cpp
-LOCAL_CLANG := false
 
 include $(BUILD_SHARED_LIBRARY)
 endif
