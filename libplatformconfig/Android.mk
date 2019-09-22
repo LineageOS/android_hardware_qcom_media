bf0d869e626401af3cb1acd01c1ef207e7c84720
Bruno Martins <bgcngm@gmail.com>
2019-09-22 11:39:01 +0100

    media: Exclude libplatformconfig from OSS builds
    It now requires requires proprietary headers
    
    Change-Id: I537a68a72eef457f2a247fc321ab2e0f42f9904b

--- libplatformconfig/Android.mk
+++ libplatformconfig/Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(QCPATH),)
+
 LOCAL_PATH := $(call my-dir)
 LOCAL_DIR_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
@@ -57,3 +59,5 @@ LOCAL_MODULE := libplatformconfig
 LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
