b5089789c053e0e65c79bb797115ddd5325d5f2a
Robert Qiu <rqiu2008@codeaurora.org>
2019-12-03 19:24:19 -0500

    sdmshirke: add configure and secomp xml files for sdmshirke platform
    Change-Id: Iafb98e92cfc24929b7ccc283ceee1ed179135baf

--- product.mk
+++ product.mk
@@ -22,7 +22,7 @@ MM_VIDEO += libstagefrighthw
 
 PRODUCT_PACKAGES += $(MM_VIDEO)
 
-ifeq ($(call is-board-platform-in-list, msmnile $(MSMSTEPPE) atoll $(TRINKET)),true)
+ifeq ($(call is-board-platform-in-list, sdmshrike msmnile $(MSMSTEPPE) atoll $(TRINKET)),true)
 include hardware/qcom/media/conf_files/$(TARGET_BOARD_PLATFORM)/$(TARGET_BOARD_PLATFORM).mk
 endif
 
