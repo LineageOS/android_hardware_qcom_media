d85171d5f3d8db4e8808f220cd2665a07cb228e9
Mahesh Lanka <mlanka@codeaurora.org>
2020-02-27 18:01:56 +0530

    mm-video-v4l2: Use LIBION_HEADER_PATH_WRAPPER for including ion.h
    - Use LIBION_HEADER_PATH_WRAPPER defined in AndroidBoardCommon.mk
    - Wrapper specifies correct path to libion and also insulates
      makefile from future path changes to libion
    
    Change-Id: I0b1dc23c4402c210010ee5c0a72cbbc544a4268e

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+include $(LIBION_HEADER_PATH_WRAPPER)
 
 # ---------------------------------------------------------------------------------
 # 				Common definitons
@@ -64,8 +65,7 @@ endif
 
 # Common Includes
 libmm-venc-inc      := $(LOCAL_PATH)/inc
-libmm-venc-inc      += $(TOP)/system/core/libion/include
-libmm-venc-inc      += $(TOP)/system/core/libion/kernel-headers
+libmm-venc-inc      += $(LIBION_HEADER_PATHS)
 libmm-venc-inc      += $(TOP)/hardware/qcom/media/mm-video-v4l2/vidc/common/inc
 libmm-venc-inc      += hardware/qcom/media/mm-core/inc
 libmm-venc-inc      += hardware/qcom/media/libstagefrighthw
