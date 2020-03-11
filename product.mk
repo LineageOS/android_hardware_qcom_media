e2d2e1a3c0634b3eecd80137be81655ad6aec997
Ashwini Muduganti <amudugan@codeaurora.org>
2020-03-11 12:38:30 -0700

    conf_files: lahaina: Add codec2 libs to PRODUCT_PACKAGES
    Add pertinent codec2 libs to PRODUCT_PACKAGES in order to pack them in
    HY11 builds by copying them to /vendor/lib
    
    Change-Id: Ib3fc349982e2e3bd3383cd037f37b7c333cbfd18

--- product.mk
+++ product.mk
@@ -3,6 +3,8 @@ ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 MM_CORE := libmm-omxcore
 MM_CORE += libOmxCore
 MM_CORE += libplatformconfig
+MM_CORE += libcodec2_vndk.vendor
+MM_CORE += libcodec2_hidl@1.0.vendor
 
 PRODUCT_PACKAGES += $(MM_CORE)
 
