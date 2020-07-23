83cd4ca246eda7111cc638038f2690ecf645a282
Paras Nagda <pnagda@codeaurora.org>
2020-07-23 16:47:10 +0530

    conf_files: trinket: Copy default profiles to odm/etc
    Copy default media_profiles.xml to odm/etc in case of
    non-ODM case.
    
    Change-Id: Ifb1753395f39b746ed3863fd007be5945d641e49

--- conf_files/trinket/trinket.mk
+++ conf_files/trinket/trinket.mk
@@ -10,7 +10,8 @@ PRODUCT_COPY_FILES += \
     $(CONFIG_PATH)/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     $(CONFIG_PATH)/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(CONFIG_PATH)/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(CONFIG_PATH)/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
+    $(CONFIG_PATH)/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml \
+    device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
 
 # Vendor property overrides
     PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank=0
