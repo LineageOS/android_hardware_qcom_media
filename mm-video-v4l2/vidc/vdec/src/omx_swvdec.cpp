93f66588d092d924fd9860dec6637ac906c99b20
Paras Nagda <pnagda@codeaurora.org>
2020-08-25 10:14:44 +0530

    mm-video : remove divx/xvid support from swcodecs
    - Remove divx/divx4/vc1 enteries from XML files.
    - Remove divx/divx4 MimeType to component name mapping
      from registry files.
    - Remove divx specific code.
    
    Change-Id: Id8adeb4681fa157a740fa775dfe236d63ae23611

--- mm-video-v4l2/vidc/vdec/src/omx_swvdec.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_swvdec.cpp
@@ -1,7 +1,7 @@
 /**
  * @copyright
  *
- *   Copyright (c) 2015-2018, The Linux Foundation. All rights reserved.
+ *   Copyright (c) 2015-2020, The Linux Foundation. All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions are met:
@@ -190,24 +190,6 @@ OMX_ERRORTYPE omx_swvdec::component_init(OMX_STRING cmp_name)
 
         m_swvdec_codec         = SWVDEC_CODEC_H263;
         m_omx_video_codingtype = OMX_VIDEO_CodingH263;
-    }
-    else if (((!strncmp(cmp_name,
-                        "OMX.qti.video.decoder.divxsw",
-                        OMX_MAX_STRINGNAME_SIZE))) ||
-             ((!strncmp(cmp_name,
-                        "OMX.qti.video.decoder.divx4sw",
-                        OMX_MAX_STRINGNAME_SIZE))))
-    {
-        OMX_SWVDEC_LOG_LOW("video_decoder.divx");
-
-        strlcpy(m_cmp_name,              cmp_name, OMX_MAX_STRINGNAME_SIZE);
-        if(!strncmp(cmp_name,"OMX.qti.video.decoder.divx4sw", OMX_MAX_STRINGNAME_SIZE))
-            strlcpy(m_role_name, "video_decoder.divx4", OMX_MAX_STRINGNAME_SIZE);
-        else
-            strlcpy(m_role_name, "video_decoder.divx", OMX_MAX_STRINGNAME_SIZE);
-
-        m_swvdec_codec         = SWVDEC_CODEC_MPEG4;
-        m_omx_video_codingtype = ((OMX_VIDEO_CODINGTYPE) QOMX_VIDEO_CodingDivx);
     }
     else if (((!strncmp(cmp_name,
                       "OMX.qti.video.decoder.vc1sw",
