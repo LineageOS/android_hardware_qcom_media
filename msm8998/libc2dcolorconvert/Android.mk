3935ac8c927e6ec4f76d76b1a7e09e3ab28c56fd
Arne Coucheron <arco68@gmail.com>
2020-11-15 01:51:38 +0100

    media: Switch to display_headers lib include
    LOCAL_COPY_HEADERS is deprecated and Qualcomm HALs have been
    updated to remove its usage. As a consequence, the display headers
    are no longer copied to TARGET_OUT_HEADERS, so use the equivalent
    header lib instead.
    
    Change-Id: Ic32a979a608bad3831bc0a2c64942f80c5028b01

--- msm8998/libc2dcolorconvert/Android.mk
+++ msm8998/libc2dcolorconvert/Android.mk
@@ -12,10 +12,12 @@ LOCAL_CFLAGS += -Wno-implicit-fallthrough
 
 LOCAL_C_INCLUDES := \
     $(TARGET_OUT_HEADERS)/adreno
-LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qcom/display
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
+
+LOCAL_HEADER_LIBRARIES := \
+        display_headers
 
 LOCAL_SHARED_LIBRARIES := liblog libdl libutils
 
