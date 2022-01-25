d4463deef10f932af5ef22fc0c67a0041e9e8ac3
Alexander Koskovich <akoskovich@pm.me>
2022-01-25 04:58:49 -0700

    media: s/PRODUCT_PROPERTY_OVERRIDES/PRODUCT_VENDOR_PROPERTIES/g
    Change-Id: I0d9c94e78349c8521b80c8ed30e4068546d41fe9

--- product.mk
+++ product.mk
@@ -15,7 +15,7 @@ PRODUCT_PACKAGES += $(MM_CORE)
 #           QSSI is forcing this to '1'. Must be reverted
 ifeq ($(call is-board-platform-in-list, bengal neo parrot taro lahaina holi), true)
     $(warning "Default Codec2.0 Enabled")
-    PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=4
+    PRODUCT_VENDOR_PROPERTIES += debug.stagefright.ccodec=4
 endif
 
 endif
