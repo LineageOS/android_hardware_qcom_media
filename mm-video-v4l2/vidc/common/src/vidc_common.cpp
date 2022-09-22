d44e43d424df67fb1f621a2569fb32f83275557f
Paras Nagda <quic_pnagda@quicinc.com>
2022-09-22 17:53:35 +0530

    mm-video-v4l2: venc: set Main10 profile
    Few CTS TC will not set the profile for
    P010 colour format, which results in failure
    of TC.
    
    Add main10 profile to list of profile
    
    Check the colour format,codec and set the
    profile for main10 to avoid cts failure.
    
    Change-Id: I9e7a057aebf34043c6f6019f0a3ef80802bb7cdf

--- mm-video-v4l2/vidc/common/src/vidc_common.cpp
+++ mm-video-v4l2/vidc/common/src/vidc_common.cpp
@@ -58,6 +58,8 @@ pl_map profile_level_converter::profile_avc_v4l2_to_omx ({});
 pl_map profile_level_converter::profile_hevc_omx_to_v4l2 ({
             {OMX_VIDEO_HEVCProfileMain,
                         V4L2_MPEG_VIDC_VIDEO_HEVC_PROFILE_MAIN},
+            {OMX_VIDEO_HEVCProfileMain10,
+                        V4L2_MPEG_VIDC_VIDEO_HEVC_PROFILE_MAIN10},
             {OMX_VIDEO_HEVCProfileMain10HDR10,
                         V4L2_MPEG_VIDC_VIDEO_HEVC_PROFILE_MAIN10},
             {OMX_VIDEO_HEVCProfileMainStill,
