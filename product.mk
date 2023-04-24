f224e84f9afe214ff859792fa70bc2be1c1c1e18
Anand Abhishek Singh <quic_anandabh@quicinc.com>
2023-04-24 17:18:49 +0530

    media: enable codec2 for kona target
    overwrite the ccodec selection option to 4 to enable codec2
    
    Change-Id: I342e633b47eb009d9cb7cda9af2ffea4ff664bb5

--- product.mk
+++ product.mk
@@ -13,7 +13,7 @@ PRODUCT_PACKAGES += $(MM_CORE)
 #---------------------------------------------------------------------------------------------------
 # TODO(PC): Override ccodec selection option back to defult (4).
 #           QSSI is forcing this to '1'. Must be reverted
-ifeq ($(call is-board-platform-in-list, crow bengal neo parrot taro lahaina holi), true)
+ifeq ($(call is-board-platform-in-list, crow bengal neo parrot taro lahaina holi kona), true)
     $(warning "Default Codec2.0 Enabled")
     PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=4
 endif
