a39dde72f64577333471c635f1915a6d55079973
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-23 18:59:43 +0800

    mm-video-v4l2: venc: Fix misleading indentation
    Change-Id: Idf025fb61a9dbf9a58adc8edad6a8341bffdc7a0

--- mm-video-v4l2/vidc/venc/src/omx_video_encoder.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_encoder.cpp
@@ -1614,7 +1614,7 @@ OMX_ERRORTYPE  omx_venc::set_parameter(OMX_IN OMX_HANDLETYPE     hComp,
                         DEBUG_PRINT_ERROR("ERROR: Request for setting PlusPType failed");
                         return OMX_ErrorUnsupportedSetting;
                     }
-                if((pParam->eHierarchicalCodingType == QOMX_HIERARCHICALCODING_B) && pParam->nNumLayers)
+                if((pParam->eHierarchicalCodingType == QOMX_HIERARCHICALCODING_B) && pParam->nNumLayers) {
                     hier_b_enabled = true;
                     m_sHierLayers.nNumLayers = pParam->nNumLayers;
                     m_sHierLayers.eHierarchicalCodingType = pParam->eHierarchicalCodingType;
