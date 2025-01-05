b88a805e0167e2b821dcaab4007c0d4e0ba6e2ac
Michael Bestas <mkbestas@lineageos.org>
2025-01-05 11:21:05 +0200

    mm-video-v4l2: Convert to blueprint
    This change required dlopening libfastcvopt.so instead of linking it
    as a shared library, since it's a proprietary device specific blob
    and soong namespaces prevent us from linking to it.
    
    Change-Id: Ib5d1c82963e5111298af391716403f1fa59934c3

--- Android.mk
+++ Android.mk
@@ -2,22 +2,9 @@ ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-VIDC_STUB_HAL := false
-ifeq ($(TARGET_USES_QMAA),true)
-  ifneq ($(TARGET_USES_QMAA_OVERRIDE_VIDEO),true)
-    VIDC_STUB_HAL := true
-  endif #TARGET_USES_QMAA_OVERRIDE_VIDEO
-endif #TARGET_USES_QMAA
-
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-endif
-
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
-ifeq ($(VIDC_STUB_HAL),false)
-include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
-endif #VIDC_STUB_HAL
 endif
 
 endif
