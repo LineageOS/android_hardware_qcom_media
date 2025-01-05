04ae4677a04d329d1a9157dc82e29ee54de7148c
Michael Bestas <mkbestas@lineageos.org>
2025-01-05 12:17:22 +0200

    mm-core: Convert to blueprint
    Change-Id: I725cd7698463691dadfcb0f7aa3bfd92d127e5ed

--- Android.mk
+++ Android.mk
@@ -3,10 +3,6 @@ ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
-include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-endif
-
 ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
