cc7368494945e312dc74124b92354ca3610224a6
Michael Bestas <mkbestas@lineageos.org>
2025-01-05 11:21:05 +0200

    mm-video-v4l2: Convert to blueprint
    Change-Id: Ib5d1c82963e5111298af391716403f1fa59934c3

--- Android.mk
+++ Android.mk
@@ -8,11 +8,8 @@ include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 endif
 
 ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
-include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
-
 ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
-
 endif
 endif
