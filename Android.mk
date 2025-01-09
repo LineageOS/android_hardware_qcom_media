81444bfeea6a4842461611a25273fd2960a3425d
Michael Bestas <mkbestas@lineageos.org>
2025-01-09 09:41:17 +0200

    libarbitrarybytes: Convert to blueprint
    Change-Id: I26f94707500ff244787b8c64118d928f0abd2fde

--- Android.mk
+++
@@ -1,9 +0,0 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
-
-QCOM_MEDIA_ROOT := $(call my-dir)
-
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
-include $(QCOM_MEDIA_ROOT)/libarbitrarybytes/Android.mk
-endif
-
-endif
