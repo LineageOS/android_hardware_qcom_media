25d01bd0a501aeb1391d4e0bc5902016653dc55a
Cosmin Tanislav <demonsingur@gmail.com>
2025-03-17 14:03:51 +0200

    media: msm8996: convert mm-core/libstagefrighthw to Android.bp
    Change-Id: Id2e150cb75e42e6740ddf35674492d7dc703aacd

--- Android.mk
+++ Android.mk
@@ -8,7 +8,12 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     QCOM_MEDIA_ROOT := $(call my-dir)/msm8998
   endif
 
-  ifneq ($(filter msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
+    include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
+    include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
+  endif
+
+  ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
     include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
     include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
